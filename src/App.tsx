b58e4555f61dd0c6d4593251d0a410b6e37b5ccf
ByeongGwan-Seo <seo.jp.work@gmail.com>
2025-07-08 14:29:26 +0900

    basic header ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./Routes/Home";
+import Tv from "./Routes/Tv";
+import Header from "./components/Header";
+
 function App() {
-  return <div> hello there</div>;
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/tv">
+          <Tv />
+        </Route>
+        <Route path="/search"></Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/Routes/Home.tsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div style={{ backgroundColor: "teal", height: "200vh" }}>
+      <h1>home</h1>
+    </div>
+  );
+}
+
+export default Home;
