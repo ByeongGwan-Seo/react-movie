75e362f20b074d24ee9d37d1731728eb00e2a44a
ByeongGwan-Seo <seo.jp.work@gmail.com>
2025-07-08 17:33:53 +0900

    header animation done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,34 @@
 import styled from "styled-components";
-import { motion } from "motion/react";
+import { motion, useAnimation, useScroll } from "motion/react";
 import { Link, useRouteMatch } from "react-router-dom";
+import { useEffect, useState } from "react";
 
-const Nav = styled.nav`
+const Nav = styled(motion.nav)`
   display: flex;
   justify-content: space-between;
   align-items: center;
   position: fixed;
   width: 100%;
   top: 0;
-  background-color: black;
   font-size: 14px;
   padding: 20px 60px;
   color: white;
 `;
+
+const navVariants = {
+  top: {
+    backgroundColor: "rgba(0,0,0,0)",
+    transition: {
+      duration: 0.3,
+    },
+  },
+  scroll: {
+    backgroundColor: "rgba(0,0,0,1)",
+    transition: {
+      duration: 0.3,
+    },
+  },
+};
 
 const Col = styled.div`
   display: flex;
@@ -25,6 +40,11 @@ const Logo = styled(motion.svg)`
   height: 20px;
   width: 120px;
 `;
+
+const logoVariants = {
+  normal: { fillOpacity: 1 },
+  active: { fillOpacity: 0 },
+};
 
 const Items = styled.ul`
   display: flex;
@@ -44,19 +64,17 @@ const Item = styled.li`
   }
 `;
 
-const logoVariants = {
-  normal: { fillOpacity: 1 },
-  active: { fillOpacity: 0 },
-};
-
 const Search = styled.span`
   color: white;
+  display: flex;
+  align-items: center;
+  position: relative;
   svg {
     height: 25px;
   }
 `;
 
-const Circle = styled.span`
+const Circle = styled(motion.span)`
   position: absolute;
   width: 5px;
   height: 5px;
@@ -68,11 +86,51 @@ const Circle = styled.span`
   background-color: ${(props) => props.theme.red};
 `;
 
+const Input = styled(motion.input)`
+  transform-origin: right center;
+  position: absolute;
+  right: 0px;
+  padding: 5px 10px;
+  padding-left: 40px;
+  z-index: -1;
+  color: white;
+  font-size: 16px;
+  background-color: transparent;
+  border: 1px solid ${(props) => props.theme.white.lighter};
+`;
+
 function Header() {
+  const [searchOpen, setSearchOpen] = useState(false);
   const homeMatch = useRouteMatch("/");
   const tvMatch = useRouteMatch("/tv");
+  const inputAnimation = useAnimation();
+  const navAnimation = useAnimation();
+  const { scrollY } = useScroll();
+  const toggleSearch = () => {
+    if (searchOpen) {
+      inputAnimation.start({
+        scaleX: 0,
+      });
+    } else {
+      inputAnimation.start({
+        scaleX: 1,
+      });
+    }
+    setSearchOpen((prev) => !prev);
+  };
+
+  useEffect(() => {
+    scrollY.on("change", () => {
+      if (scrollY.get() > 80) {
+        navAnimation.start("scroll");
+      } else {
+        navAnimation.start("top");
+      }
+    });
+  }, [scrollY, navAnimation]);
+
   return (
-    <Nav>
+    <Nav variants={navVariants} initial={"up"} animate={navAnimation}>
       <Col>
         <Logo
           variants={logoVariants}
@@ -90,15 +148,40 @@ function Header() {
         </Logo>
         <Items>
           <Item>
-            <Link to="/">Home {homeMatch?.isExact && <Circle />}</Link>
+            <Link to="/">
+              Home {homeMatch?.isExact && <Circle layoutId="circle" />}
+            </Link>
           </Item>
           <Item>
-            <Link to="/tv">Tv Shows {tvMatch && <Circle />}</Link>
+            <Link to="/tv">
+              Tv Shows {tvMatch && <Circle layoutId="circle" />}
+            </Link>
           </Item>
         </Items>
       </Col>
       <Col>
-        <button>Search</button>
+        <Search>
+          <motion.svg
+            onClick={toggleSearch}
+            animate={{ x: searchOpen ? -215 : 0 }}
+            transition={{ type: "tween", ease: "linear", duration: 0.1 }}
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+              clipRule="evenodd"
+            ></path>
+          </motion.svg>
+          <Input
+            animate={inputAnimation}
+            initial={{ scaleX: 0 }}
+            transition={{ type: "tween", ease: "linear", duration: 0.1 }}
+            placeholder="Search for movie or tv show..."
+          />
+        </Search>
       </Col>
     </Nav>
   );
